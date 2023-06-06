10ca11e0448d318da9dee4cb9b63ba17cda8a914
王文成 <dcz6666@126.com>
2023-06-06 08:43:24 +0800

    表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,7 +4,7 @@
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
         <!-- 登录的表单 -->
-        <el-form class="login_form" :model="loginForm" ref="loginForms">
+        <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
           <h1>Hello</h1>
           <h2>欢迎来到硅谷甄选</h2>
           <el-form-item prop="username">
@@ -31,10 +31,12 @@ import { useRouter } from 'vue-router'
 import { ElNotification } from 'element-plus'
 import { ref, reactive } from 'vue'
 //引入获取当前时间
-import {getTime} from '@/utils/time'
+import { getTime } from '@/utils/time'
 let useStore = useUserStore()
 //定义变量控制按钮加载效果
 let loading = ref(false)
+//获取el-from组件
+let loginForms = ref()
 //获取路由器
 let $router = useRouter()
 //收集账号与密码的数据
@@ -42,6 +44,9 @@ let loginForm = reactive({ username: 'admin', password: 'atguigu123' })
 
 //登录按钮回调
 const login = async () => {
+  let a = await loginForms.value.validate()
+    console.log("loginForms1",a);
+
   loading.value = true
   try {
     await useStore.userLogin(loginForm)
@@ -51,7 +56,7 @@ const login = async () => {
     ElNotification({
       type: 'success',
       message: '欢迎回来',
-      title:`Hi,${getTime()}好`
+      title: `Hi,${getTime()}好`,
     })
   } catch (error) {
     ElNotification({
@@ -63,6 +68,15 @@ const login = async () => {
   }
 }
 
+//定义表单校验需要配置对象
+const rules = {
+  username: [
+    { min: 6, max: 10, message: '账号长度至少6位', trigger: 'change' },
+  ],
+  password: [
+    { min: 6, max: 15, message: '密码长度至少6位', trigger: 'change' },
+  ],
+}
 </script>
 <style lang="scss" scoped>
 .login_container {
